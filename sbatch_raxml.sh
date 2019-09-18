c72afc51781b809fe4725a94c08d095ea2cbd480
Chao Tong <33243479+jiyideanjiao@users.noreply.github.com>
2019-09-17 20:43:43 -0400

    Update sbatch_raxml.sh

--- sbatch_raxml.sh
+++ sbatch_raxml.sh
@@ -3,6 +3,16 @@
 #SBATCH -p compute # partition (queue)
 #SBATCH --export=ALL
 #SBATCH -t 20-00:00
+#SBATCH --mail-user=tongchao1990@gmail.com
 #SBATCH -n 15
+
+sh raxml.sh
+
+
+#!/bin/bash -ve
+
+#SBATCH -p compute # partition (queue)
+#SBATCH --export=ALL
+#SBATCH -n 1
 
 sh raxml.sh
